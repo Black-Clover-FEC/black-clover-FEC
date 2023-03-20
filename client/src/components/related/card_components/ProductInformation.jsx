fe519ae511679cf7138ac6f498bb77f2be2573be
Brandon Woods <woods.brandon.m@gmail.com>
2023-03-20 15:49:37 -0700

    Create basic RelatedProductCard component (dummy data)

---
+++ client/src/components/related/card_components/ProductInformation.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// TODO - product information for RelatedProductCards
+
+const ProductInfo = () => {
+
+  return (
+    <div>
+      <h5>Category</h5>
+      <h4>Expanded Product Name with Extra Text</h4>
+      <h4>Price</h4>
+
+    </div>
+  )
+}
+
+export default ProductInfo;
