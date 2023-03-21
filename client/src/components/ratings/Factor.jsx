76953c25ef19c0d757d36b206c2b16717401c127
Johnathan Simeroth <john.simeroth@gmail.com>
2023-03-21 11:26:24 -0700

    convert data provided to rating breakdown and product factor breakdown into visual representations

--- client/src/components/ratings/Factor.jsx
+++ client/src/components/ratings/Factor.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import StyleLib from '../../assets/Stylesheet.jsx';
+import styled from 'styled-components';
+
+const width = 10; // units: em;
+const maxScore = 5;
+
+const StyledDiv = styled.div`
+background-color: #D5D5D5;
+height: 0.7em;
+width: 10em;
+position: relative;
+`
+
+const StyledSpan = styled.span.attrs(props => ({
+  left: props.left || '0'
+}))`
+color: black;
+position: absolute;
+left: ${props => props.left}%;
+bottom: 40%;
+transform: translateX(-50%);
+`
 
 
 const Factor = ({factor, value}) => {
 
+  value = Number.parseFloat(value).toFixed(1)
+  const left = Math.round((value / maxScore) * 100);
+  console.log(left);
+
   return (
-    <li>{factor}: {value}</li>
+    <div>
+      {factor}: {value}
+      <StyledDiv>
+        <StyledSpan left={left}>⌄</StyledSpan>
+      </StyledDiv>
+    </div>
   );
 }
 
