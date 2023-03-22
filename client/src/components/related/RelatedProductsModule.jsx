da3f9a5ec80c4a2460dad9c16bbe43ef234efc88
Brandon Woods <woods.brandon.m@gmail.com>
2023-03-22 10:50:16 -0700

    Aggregate data for product and add to related items list

--- client/src/components/related/RelatedProductsModule.jsx
+++ client/src/components/related/RelatedProductsModule.jsx
@@ -6,67 +6,77 @@ import api from '../../../../client/src/lib/api.js';
 const RelatedProductsModule = () => {
   // represents a future state for current selected product
   let testCurrentProduct = 40344;
-
-  const [relatedItems, setRelatedItems] = useState(['no related products']);
-  const [relatedItemIds, setRelatedItemIds] = useState(['no id']);
   // TODO - outfit list not used yet
   const [outfitList, setOutfitList] = useState([]);
+  const [relatedItems, setRelatedItems] = useState([]);
 
-  const getRelatedIds = () => {
-    api.getRelatedProducts(testCurrentProduct)
-    .then((res) => {
-      setRelatedItemIds(res);
+  // States for each api call
+  const [relatedItemId, setRelatedItemId] = useState(['placeholder']);
+  const [itemStyle, setItemStyle] = useState();
+  const [itemDetails, setItemDetails] = useState([]);
+
+  // get list of related products by id
+  const getRelatedIds = (productId) => {
+    api.getRelatedProducts(productId)
+    .then((data) => {
+      setRelatedItemId(data);
     })
     .catch((err) => {
-      console.log('get product failed', err);
+      console.log('failed to get related product ids', err);
     })
   }
 
-  const getStaticInfo = () => {
-    api.getProductId(productId)
-    .then((res) => {
-      console.log('results for id', res);
+  // get details for product
+  const getDetails = (productId) => {
+    return api.getProductId(productId)
+    .then((data) => {
+      // setItemDetails(data);
+      console.log('details', data);
+      return data;
     })
-    .then(() => {
-      console.log('product static data', productStaticData);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+    .catch(err => console.log(err))
   }
 
-  // const getImageAndPrice = () => {
-  //   api.getProductStyles(productId)
-  //   .then()
-  //   .catch()
-  // }
-
-  const populateRelatedItems = () => {
-    // iterate over related product id's
-    let list = [];
-    for (let i = 0; i < relatedItemIds.length; i++) {
-      let productId = relatedItemIds[i];
-      let product = {};
-      product.staticInfo = getStaticInfo(productId);
-    }
-    // run getStaticInfo to get name and category
-    // run getImageAndPrice
-    // run getRating
-
-
-    // set related items to array of objects
-    setRelatedItems(list);
+  // get styles for product
+  const getStyles = (productId) => {
+   return api.getProductStyles(productId)
+    .then((data) => {
+      // setItemStyle(data);
+      console.log('styles', data);
+      return data;
+    })
+    .catch(err => console.log(err))
   }
 
-  // TODO - change dependency to update when current product changes
-  useEffect(getRelatedIds, []);
+  // use api calls to aggregate data in to object
+  const collectProductInfo = (id) => {
+    let product = {};
+
+    api.getProductId(id)
+    .then((data) => {
+      product.details = data;
+      return api.getProductStyles(id);
+    })
+    .then((data) => {
+      product.styles = data;
+      return api.getReviewsMetadata({product_id: id});
+    })
+    .then((data) => {
+      product.reviewsMeta = data;
+      setRelatedItems(relatedItems.concat(product));
+      console.log('product: ', product);
+    })
+    .catch(err => console.log(err));
+  }
+
+  useEffect(() => {collectProductInfo(testCurrentProduct)}, []);
 
   return (
     <div>
       Hello World, this is where Related Products go!
       <div>
         {/* TODO - update related items prop after related product objects are formed */}
-        <RelatedProductList relatedItems={relatedItemIds}/>
+        <RelatedProductList relatedItems={relatedItems} />
       </div>
       <div>
         <YourOutfitList />
