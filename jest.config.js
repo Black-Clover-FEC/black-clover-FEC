57e45ebd9288598eeb043a1de317b7de8142f70c
Brandon Woods <woods.brandon.m@gmail.com>
2023-03-22 23:01:26 -0700

    make jest configurations for webpack

--- jest.config.js
+++ jest.config.js
@@ -71,27 +71,25 @@ module.exports = {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  // moduleDirectories: [
-  //   "node_modules"
-  // ],
+  moduleDirectories: [
+    "node_modules"
+  ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: [
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
     '<rootDir>/__mocks__/fileMock.js',
-  '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '^react(.*)$': '<rootDir>/vendor/react-master$1',
+    '^config$': '<rootDir>/configs/app-config.js',
 },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
