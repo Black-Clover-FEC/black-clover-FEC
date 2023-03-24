475ca75d6679b78aea4fdb2d26647945d9810b8c
Brandon Woods <woods.brandon.m@gmail.com>
2023-03-23 21:23:20 -0700

    create all features list

--- client/src/components/related/modal/ComparisonModal.jsx
+++ client/src/components/related/modal/ComparisonModal.jsx
@@ -10,18 +10,32 @@ Modal.setAppElement('#root');
 
 const ComparisonModal = ({features, isOpen, onClose}) => {
   // compare characteristics of list with characteristics for product - apply to both products
-  console.log(features);
+  const [allFeatures, setAllFeatures] = useState([]);
+  // generate combined list of both feature lists, no duplicates
+
+  const combineFeatureList = () => {
+    let combined = [...features[0], ...features[1]];
+    for (let k = 0; k < combined.length; k++) {
+      for (let i = k + 1; i < combined.length; i++) {
+        if (combined[k].feature === combined[i].feature) {
+          combined.splice(i--, 1);
+        }
+      }
+    }
+    setAllFeatures(combined);
+  }
+
 
   return (
 
     <Modal
     isOpen={isOpen}
+    onAfterOpen={combineFeatureList}
     onRequestClose={onClose}
     >
 
       <span>Comparing</span>
       <div className='modal-header'>
-
         <div>features go here</div>
       </div>
       <div className='modal-body'>
