003ac5b4b9a86a006c2808bb8af79e5646fd6cab
Brandon Woods <woods.brandon.m@gmail.com>
2023-03-27 11:30:15 -0700

    Pass first test for and generate coverage report

--- client/src/components/related/__tests__/AddToOutfitCard.test.jsx
+++ client/src/components/related/__tests__/AddToOutfitCard.test.jsx
@@ -1,11 +1,14 @@
 import React from 'react'; // import like any other jsx file
 import renderer from 'react-test-renderer';
 import {cleanup, fireEvent, render} from '@testing-library/react';  // used to assert and manipulate rendered components
-import ComparisonModal from '../client/src/components/related/ComparisonModal.jsx';
+import AddToOutfitCard from '../card_components/AddToOutfitCard.jsx';
 
 // for more info on testing with jest and react see, https://jestjs.io/docs/tutorial-react
 
-it('test to see if the related module renders correctly', () => {
-  const tree = renderer.create(<ComparisonModal />).toJSON();
-  expect(tree).toMatchShapshot();
+// EXAMPLE TEST CASES
+
+// TEST CHANGES TO COMPONENT RENDERING
+it('test to see if the AddToOutfitCard component renders correctly', () => {
+  const tree = renderer.create(<AddToOutfitCard />).toJSON();
+  expect(tree).toMatchSnapshot();
 })
