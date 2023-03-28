ce1c466b540c2d772308e0957c6aad483af4848d
Noah Clouthier <noahclouthier@Noahs-MacBook-Air.local>
2023-03-28 13:56:53 -0700

    Fixed see more answers/collapse answer button

--- client/src/components/questions/AnswerList.jsx
+++ client/src/components/questions/AnswerList.jsx
@@ -7,29 +7,48 @@ const AnswerList = ({answers, helpfulCB}) => {
   const answersArray = Object.keys(answers);
   const [displayedAnswers, setDisplayedAnswers] = useState([]);
   const [numberOfAnswers, setNumberOfAnswers] = useState(2);
+  const [buttonText, setButtonText] = useState('');
 
   useEffect(() => {setDisplayedAnswers(answersArray.slice(0, 2))}, []);
-  useEffect(() => {updateDisplayedAnswers}, [numberOfAnswers]);
 
-  const updateDisplayedAnswers = () => {
-    setDisplayedAnswers(answersArray.slice(0, numberOfAnswers));
-  }
+  useEffect(() => {
+    updateDisplayedAnswers();
+  }, [numberOfAnswers]);
+
+  useEffect(() => {
+    buttonTextHelper();
+  }, [displayedAnswers]);
 
   const render = (answer, index) => {
     return <Answer answer={answer} helpfulCB={helpfulCB} key={index}/>
   }
 
+  const updateDisplayedAnswers = () => {
+    setDisplayedAnswers(answersArray.slice(0, numberOfAnswers));
+  }
+
   const updateNumberOfAnswers = (e) => {
-    console.log(displayedAnswers);
-    setNumberOfAs(answersArray.length);
+    if (numberOfAnswers < answersArray.length) {
+      setNumberOfAnswers(answersArray.length);
+    } else {
+      setNumberOfAnswers(2);
+    };
+
   };
-  const resetNumberOfAnswers = (e) => {setNumberOfAnswers(2)};
+
+  const buttonTextHelper = () => {
+    if (displayedAnswers.length < answersArray.length) {
+      setButtonText('See More Answers');
+    }
+    if (displayedAnswers.length >= answersArray.length) {
+      setButtonText('Collapse Answers');
+    }
+  }
 
   return (
     <section>
       {displayedAnswers.map((answer, index) => render(answers[answer], index))}
-      {answersArray.length > numberOfAnswers ? <StyleLib.button onClick={updateNumberOfAnswers}>See more answers</StyleLib.button> : null}
-      {/* {answersArray.length <= numberOfAnswers && answersArray.length !== 0 && numberOfAs !== 2 ? <StyleLib.button onClick={resetNumberOfAnswers}>Collapse Answers</StyleLib.button> : null} */}
+      {answersArray.length > 2 && <StyleLib.button onClick={updateNumberOfAnswers}>{buttonText}</StyleLib.button>}
     </section>
   )
 }
